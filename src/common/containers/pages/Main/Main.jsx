738b4abaa872e62999ec13fda3545e5bd77f9c6b
Anton Mikutsevich <Anton_Mikutsevich@epam.com>
2017-10-02 11:18:23 +0300

    Add Main page

---
+++ src/common/containers/pages/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Main = () => (
+  <div>
+    <Link to='/counter'>Counter</Link>
+    <h1>The main page</h1>
+  </div>
+);
+
+export default Main;
