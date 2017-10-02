09470bb300fd1598781639e3b787f53100fe5209
Anton Mikutsevich <Anton_Mikutsevich@epam.com>
2017-10-02 21:17:19 +0300

    Update Couner component.

--- src/common/components/Counter/index.js
+++ src/common/components/Counter/index.js
@@ -1,7 +1,26 @@
+import React, { Component } from 'react';
+
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Counter from './Counter.jsx';
 import * as CounterActions from '../../actions/counter';
+
+import { default as CounterComponent } from './Counter.jsx';
+
+class Counter extends Component {
+  static initialAction() {
+    return CounterActions.fetchCounter();
+  }
+
+  componentDidMount() {
+    if (!this.props.counter) {
+      this.props.dispatch(Counter.initialAction());
+    }
+  }
+
+  render() {
+    return <CounterComponent {...this.props} />;
+  }
+}
 
 const mapStateToProps = (state) => ({
   counter: state.counter
